1757b82e458bea1b7c265ff06f6ed0c5d2baa992
jakeia <azbazb011@gmail.com>
2024-10-07 17:34:01 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,5 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>q", ":wqa<CR>", { silent = true })
 map("n", "H", "^")
 map("n", "L", "$")
+map("n", "<leader>gg", ":LazyGit<CR>", { desc = "LazyGit", silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
