3c75ca44f0f409efcfe652879836617321a87ab1
jakeia <azbazb011@gmail.com>
2024-10-21 21:39:14 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -18,7 +18,7 @@ M.ui = {
   cmp = {
     icons_left = false, -- only for non-atom styles!
     lspkind_text = true,
-    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    style = "flat_light", -- default/flat_light/flat_dark/atom/atom_colored
     format_colors = {
       tailwind = false, -- will work for css lsp too
       icon = "󱓻",
@@ -29,6 +29,14 @@ M.ui = {
     enabled = true,
     lazyload = true,
     order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
     modules = nil,
   },
 }
