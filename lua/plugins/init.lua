f11a81e9a6b3b02d8debfe66a9e0c6c7539e408a
jakeia <azbazb011@gmail.com>
2024-12-01 18:24:32 +0800

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,30 @@
 return {
   {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>cy",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
+  {
     "christoomey/vim-tmux-navigator",
     event = "VeryLazy",
     cmd = {
