b80fcc77b9b53ef268df7b240cea9507ad4aad63
jakeia <azbazb011@gmail.com>
2025-01-02 16:07:22 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -33,7 +33,7 @@ M.ui = {
     modules = nil,
   },
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "default",
@@ -44,42 +44,6 @@ M.ui = {
 
 M.nvdash = {
   load_on_startup = true,
-  header = {
-    "                            ",
-    "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
-    "   ▄▀███▄     ▄██ █████▀    ",
-    "   ██▄▀███▄   ███           ",
-    "   ███  ▀███▄ ███           ",
-    "   ███    ▀██ ███           ",
-    "   ███      ▀ ███           ",
-    "   ▀██ █████▄▀█▀▄██████▄    ",
-    "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
-    "                            ",
-    "     Powered By  eovim    ",
-    "                            ",
-  },
-
-  buttons = {
-    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
-    { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
-    { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
-    { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
-    { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
-
-    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-
-    {
-      txt = function()
-        local stats = require("lazy").stats()
-        local ms = math.floor(stats.startuptime) .. " ms"
-        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-      end,
-      hl = "NvDashLazy",
-      no_gap = true,
-    },
-
-    { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-  },
 }
 
 M.lsp = {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local del = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>q", "<cmd>wqa<cr>", { desc = "Quit and save", silent = true })
@@ -10,6 +11,7 @@ map("n", "H", "^")
 map("n", "L", "$")
 map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit", silent = true })
 map("n", "<leader>sk", "<cmd>ShowkeysToggle<cr>", { desc = "ShowkeysToggle", silent = true })
+map("n", "<leader>ww", "<cmd>w!<cr>", { desc = "Save", silent = true })
 -- menu keymap
 map("n", "<RightMouse>", function()
   vim.cmd.exec '"normal! \\<RightMouse>"'
@@ -20,5 +22,6 @@ end, {})
 map("n", "<C-t>", function()
   require("menu").open "default"
 end, {})
-
-map("n", "<leader>ww", "<cmd>w!<cr>", { silent = true })
+-- del
+del("n", "<leader>wk")
+del("n", "<leader>wK")
