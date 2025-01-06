cb083db6581e90d1b3959683a5824ab18a4ec988
jakeia <azbazb011@gmail.com>
2025-01-06 20:29:27 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,7 +5,7 @@
 ---@type ChadrcConfig
 local M = {}
 M.base46 = {
-  theme = "one_light",
+  theme = "github_light",
   hl_override = {
     -- Comment = { italic = true },
     ["@comment"] = { italic = true },
